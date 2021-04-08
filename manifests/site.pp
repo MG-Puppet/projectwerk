74107170652a8742027cddefef23010093e88f70
root <root@ubnt-pps.mitchell.test>
2021-04-08 21:57:33 +0200

    Created SSH Module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,25 @@
 node 'srv19-dc1.mitchell.test' {
 
-   # include chocolatey
-   
+   include chocolatey
+   include ssh
+}
+
+node 'w10-puppet' {
+
+  include chocolatey
+  
+  include ssh
+
+}
+
+node 'centos-agent.mitchell.test' {
+
+  include ssh  
+
+}
+
+node 'ubnt-agent.mitchell.test' {
+
+  include ssh
+
 }
